3fa3dbb2ff5c87ef09e40636ee41a5671c9fc2f8
Jay <logikal@gmail.com>
2015-01-10 20:25:48 +1300

    working on run controller, adding zones and days

--- controllers/run.js
+++ controllers/run.js
@@ -8,8 +8,30 @@ module.exports = function(app) {
     });
 
     app.get('/run/create', app.libs.restrict, function(req, res) {
-        var zones = [];
-        var users = [];
+        var zones = [
+        {
+            name: 'zone1',
+            days: [
+            {
+                name: "1",
+                amount: 10
+            }]
+        },{
+            name:'zone2',
+            days: [
+            {
+                name: "1",
+                amount: 10
+            },{
+                name: "2",
+                amount: 5
+            }, {
+                name: "3",
+                amount: 7
+            }]
+        }];
+
+        var users = ['jay', 'bob', 'jim', 'paul', 'smith'];
 
         res.render('run/create', { zones: zones, users: users });
     });
