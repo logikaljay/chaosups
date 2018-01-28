afb632354f40b22a11f2536ca18475c7a03458c6
Jay Baker <logikal@gmail.com>
2018-01-29 09:17:32 +1300

    fix: include crypto digest

--- app.js
+++ app.js
@@ -1,105 +1,107 @@
 // ChaosUPS - a universal points system.
 // Copyright (C) 2015  Jay Baker
-	
+
 /**
  * Module dependencies.
  */
 
-var fs = require('fs')
-  , path = require('path')
-  , mongoose = require('mongoose')
-  , express = require('express')
-  , SessionStore = require('session-mongoose')(express)
-  , bodyParser = require('body-parser')
-  , engine = require('ejs-locals')
-  , moment = require('moment')
-  , async = require('async')
-  , hash = require('./lib/hash').hash;
+var fs = require('fs'),
+  path = require('path'),
+  mongoose = require('mongoose'),
+  express = require('express'),
+  SessionStore = require('session-mongoose')(express),
+  bodyParser = require('body-parser'),
+  engine = require('ejs-locals'),
+  moment = require('moment'),
+  async = require('async'),
+  hash = require('./lib/hash').hash
 
-mongoose.connect('mongodb://localhost/chaosups');
-var app = module.exports = express();
+mongoose.connect('mongodb://localhost/chaosups')
+var app = (module.exports = express())
 
 // Load libraries
-app.libs = {};
-var libsDir = path.join(__dirname, 'lib');
-var libs = fs.readdirSync(libsDir);
+app.libs = {}
+var libsDir = path.join(__dirname, 'lib')
+var libs = fs.readdirSync(libsDir)
 libs.forEach(function(lib) {
-    console.log(lib);
-    require(path.join(libsDir, lib))(app);
-});
+  console.log(lib)
+  require(path.join(libsDir, lib))(app)
+})
 
 // Load models
-app.models = {};
-var modelsDir = path.join(__dirname, 'models');
+app.models = {}
+var modelsDir = path.join(__dirname, 'models')
 fs.readdir(modelsDir, function(err, files) {
-    files.forEach(function(model) {
-        require(path.join(__dirname, 'models', model))(app);
-    });
-});
+  files.forEach(function(model) {
+    require(path.join(__dirname, 'models', model))(app)
+  })
+})
 
 // Load controllers
-app.controllers = {};
-var controllersDir = path.join(__dirname, 'controllers');
+app.controllers = {}
+var controllersDir = path.join(__dirname, 'controllers')
 fs.readdir(controllersDir, function(err, files) {
-    files.forEach(function(controller) {
-        console.log(controller);
-        require(path.join(controllersDir, controller))(app);
-    });
-});
+  files.forEach(function(controller) {
+    console.log(controller)
+    require(path.join(controllersDir, controller))(app)
+  })
+})
 
 // Load factories
-app.factory = {};
-var factoryDir = path.join(__dirname, 'factory');
+app.factory = {}
+var factoryDir = path.join(__dirname, 'factory')
 fs.readdir(factoryDir, function(err, files) {
-    files.forEach(function(factory) {
-        console.log(factory);
-        require(path.join(factoryDir, factory))(app);
-    });
-});
+  files.forEach(function(factory) {
+    console.log(factory)
+    require(path.join(factoryDir, factory))(app)
+  })
+})
 
 // Load plugins
-app.plugins = [];
-var pluginsDir = path.join(__dirname, 'plugins');
+app.plugins = []
+var pluginsDir = path.join(__dirname, 'plugins')
 fs.readdir(pluginsDir, function(err, plugins) {
   plugins.forEach(function(plugin) {
     if (plugin.indexOf('.js') > 0) {
-      require(path.join(pluginsDir, plugin))(app);
-      console.log("[PLUGIN] Loaded: %s", plugin);
+      require(path.join(pluginsDir, plugin))(app)
+      console.log('[PLUGIN] Loaded: %s', plugin)
     }
-  });
-});
+  })
+})
 
 // Session store configuration
 var sessionStore = new SessionStore({
-    interval: 120000, // expiration check worker run interval in millisec (default: 60000)
-    modelName: "session",
-    connection: mongoose.connection // <== custom connection
-});
+  interval: 120000, // expiration check worker run interval in millisec (default: 60000)
+  modelName: 'session',
+  connection: mongoose.connection // <== custom connection
+})
 
 // Set local variables views
-app.locals.moment = moment;
-app.locals.title = "ChaosUPS";
+app.locals.moment = moment
+app.locals.title = 'ChaosUPS'
 
 // Configuration
-app.engine('ejs', engine);
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('shhhhh, 1234 secrets inside'));
-app.use(express.session({
-  secret: 'shhhh, 1234 secrets inside',
-  store: sessionStore,
-  cookie: { maxAge: 15 * 60 * 1000 }
-}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
+app.engine('ejs', engine)
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.bodyParser())
+app.use(express.methodOverride())
+app.use(express.cookieParser('shhhhh, 1234 secrets inside'))
+app.use(
+  express.session({
+    secret: 'shhhh, 1234 secrets inside',
+    store: sessionStore,
+    cookie: { maxAge: 15 * 60 * 1000 }
+  })
+)
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(app.router)
 
-app.listen(3000, function(){
-  console.log("Express server listening");
-});
+app.listen(80, function() {
+  console.log('Express server listening')
+})
--- tools/adduser.js
+++ tools/adduser.js
@@ -1,39 +1,39 @@
 // ChaosUPS - a universal points system.
 // Copyright (C) 2015  Jay Baker
 
-var mongoose = require('mongoose');
-var util = require('util');
+var mongoose = require('mongoose')
+var util = require('util')
 
-var app = { factory: {}, models: {}, libs: {} };
-require('../models/user.js')(app);
-require('../factory/users.js')(app);
-require('../lib/hash.js')(app);
+var app = { factory: {}, models: {}, libs: {} }
+require('../models/user.js')(app)
+require('../factory/users.js')(app)
+require('../lib/hash.js')(app)
 
-mongoose.connect('mongodb://localhost/chaosups');
+mongoose.connect('mongodb://localhost/chaosups')
 
 if (process.argv.length <= 2) {
-    console.log("Usage: nodejs adduser.js [username]");
-    return process.exit(0);
+  console.log('Usage: nodejs adduser.js [username]')
+  return process.exit(0)
 }
 
-var name = process.argv[2];
-var type = process.argv[3] || 0;
+var name = process.argv[2]
+var type = process.argv[3] || 0
 app.factory.users.exists(name, function(exists) {
-    if (exists) {
-        console.log(util.format("The user '%s' already exists", name));
-        return process.exit(0);
-    }
+  if (exists) {
+    console.log(util.format("The user '%s' already exists", name))
+    return process.exit(0)
+  }
 
-    app.factory.users.add(name, function(userEntity) {
-        userEntity.type = type;
-        userEntity.save(function(err) {
-            if (type == 0) {
-                console.log(util.format("The user '%s' was created", name));
-            } else if (type == 1) {
-                console.log(util.format("The gatekeeper '%s' was created", name));
-            } else if (type == 2) {
-                console.log(util.format("The admin '%s' was created", name));
-            }
-        });
-    });
-});
+  app.factory.users.add(name, function(userEntity) {
+    userEntity.type = type
+    userEntity.save(function(err) {
+      if (type == 0) {
+        console.log(util.format("The user '%s' was created", name))
+      } else if (type == 1) {
+        console.log(util.format("The gatekeeper '%s' was created", name))
+      } else if (type == 2) {
+        console.log(util.format("The admin '%s' was created", name))
+      }
+    })
+  })
+})
