790905bfcf0563abe9656cd547c505696115a6d7
Jay Baker <logikal@gmail.com>
2018-02-06 15:09:01 +1300

    fix(items): order sent/unset items correctly

--- controllers/item.js
+++ controllers/item.js
@@ -1,78 +1,108 @@
 // ChaosUPS - a universal points system.
 // Copyright (C) 2015  Jay Baker
 
-var util = require('util')
-  , async = require('async');
+var util = require('util'),
+  async = require('async')
 
 module.exports = function(app) {
-    app.get('/items/list', app.libs.restrict, function(req, res) {
-        var userId = req.session.user.id;
+  app.get('/items/list', app.libs.restrict, function(req, res) {
+    var userId = req.session.user.id
 
-        // get items, and points
-        app.factory.items.getByState(0, function(items) {
-            app.factory.points.getAllByUserId(userId, function(points) {
-                app.factory.items.getWonByUserId(userId, function(wonItems) {
-                    res.render('item/list', {
-                        items: items,
-                        points: points,
-                        wonItems: wonItems,
-                        itemId: null });
-                });
-            });
-        });
-    });
+    // get items, and points
+    app.factory.items.getByState(0, function(items) {
+      app.factory.points.getAllByUserId(userId, function(points) {
+        app.factory.items.getWonByUserId(userId, function(wonItems) {
+          res.render('item/list', {
+            items: items,
+            points: points,
+            wonItems: wonItems,
+            itemId: null
+          })
+        })
+      })
+    })
+  })
 
-    app.get('/items/list/:itemId', app.libs.restrict, function(req, res) {
-        var userId = req.session.user.id;
-        var itemId = req.params.itemId;
+  app.get('/items/list/:itemId', app.libs.restrict, function(req, res) {
+    var userId = req.session.user.id
+    var itemId = req.params.itemId
 
-        // get items, and points
-        app.factory.items.getByState(0, function(items) {
-            app.factory.points.getAllByUserId(userId, function(points) {
-                app.factory.items.getWonByUserId(userId, function(wonItems) {
-                    res.render('item/list', {
-                        items: items,
-                        points: points,
-                        wonItems: wonItems,
-                        itemId: itemId });
-                });
-            });
-        });
-    });
+    // get items, and points
+    app.factory.items.getByState(0, function(items) {
+      app.factory.points.getAllByUserId(userId, function(points) {
+        app.factory.items.getWonByUserId(userId, function(wonItems) {
+          res.render('item/list', {
+            items: items,
+            points: points,
+            wonItems: wonItems,
+            itemId: itemId
+          })
+        })
+      })
+    })
+  })
 
-    app.get('/items/unsent', app.libs.restrictAdmin, function(req, res) {
-        app.factory.items.getByState(2, function(items) {
-            res.render('item/unsent', { items: items });
-        });
-    });
+  app.get('/items/unsent', app.libs.restrictAdmin, function(req, res) {
+    app.factory.items.getByState(2, function(items) {
+      items = items.sort((a, b) => {
+        if (a.currentBid.endDate < b.currentBid.endDate) {
+          return -1
+        } else if (b.currentBid.endDate < a.currentBid.endDate) {
+          return 1
+        }
 
-    app.get('/items/sent', app.libs.restrictAdmin, function(req, res) {
-        app.factory.items.getByState(3, function(items) {
-            res.render('item/sent', { items: items });
-        });
-    });
+        return 0
+      })
 
-    app.get('/items/send/:id', app.libs.restrictAdmin, function(req, res) {
-        var itemId = req.params.id;
+      res.render('item/unsent', { items: items })
+    })
+  })
 
-        // set the item state to 3
-        app.factory.items.getById(itemId, function(item) {
-            item.state = 3;
-            item.save(function(err) {
-                res.redirect('back');
-            });
-        });
-    });
+  app.get('/items/sent', app.libs.restrictAdmin, function(req, res) {
+    app.factory.items.getByState(3, function(items) {
+      // console.log(items)
+      items = items.sort((a, b) => {
+        if (a.currentBid.endDate > b.currentBid.endDate) {
+          return -1
+        } else if (b.currentBid.endDate > a.currentBid.endDate) {
+          return 1
+        }
 
-    app.get('/items/unsend/:id', app.libs.restrictAdmin, function(req, res) {
-        var itemId = req.params.id;
+        return 0
+      })
+      res.render('item/sent', { items: items })
+    })
+  })
 
-        // set the item state to 2
-        app.factory.items.getById(itemId, function(item) {
-            item.state = 2;
-            item.save(function(err) {
-                res.redirect('back');
-            });
-        });
-    });
+  app.get('/items/send/:id', app.libs.restrictAdmin, function(req, res) {
+    var itemId = req.params.id
+
+    // set the item state to 3
+    app.factory.items.getById(itemId, function(item) {
+      item.state = 3
+      item.save(function(err) {
+        res.redirect('back')
+      })
+    })
+  })
+
+  app.get('/items/unsend/:id', app.libs.restrictAdmin, function(req, res) {
+    var itemId = req.params.id
+
+    // set the item state to 2
+    app.factory.items.getById(itemId, function(item) {
+      item.state = 2
+      item.save(function(err) {
+        res.redirect('back')
+      })
+    })
+  })
+
+  app.get('/items/similar/:name', app.libs.restrict, function(req, res) {
+    var name = req.params.name
+
+    app.factory.items.getByName(name, items => {
+      res.json(items)
+    })
+  })
 }
